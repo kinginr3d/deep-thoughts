ae8491ad0015ddf5a8249c62bb4196dfa3389a9f
Jake Byers <jake.e.byers@gmail.com>
2023-07-19 18:27:17 -0500

    Added Mutations, and JWT Authentication.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -22,6 +22,11 @@ const typeDefs = gql`
     friends: [User]
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Reaction {
     _id: ID
     reactionBody: String
@@ -30,11 +35,21 @@ const typeDefs = gql`
   }
 
   type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+  }
+
 `;
 
 // export the typeDefs
